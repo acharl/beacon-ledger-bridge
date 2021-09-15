bf7fdc45b2efea5cc698ab6d4dfac757dc91ddb7
acharl <amadeo.charle108@gmail.com>
2021-09-15 14:51:13 +0200

    chore(): DEV

--- src/ledger-bridge.js
+++ src/ledger-bridge.js
@@ -3,6 +3,7 @@
 import WebSocketTransport from '@ledgerhq/hw-transport-http/lib/WebSocketTransport'
 import Tezos from '@obsidiansystems/hw-app-xtz'
 import TransportWebHID from '@ledgerhq/hw-transport-webhid'
+import regeneratorRuntime from 'regenerator-runtime'
 
 // const Tezos = require('@ledgerhq/hw-app-xtz').default
 
